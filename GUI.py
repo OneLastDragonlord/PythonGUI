97bf3cd6f91f2d2fd7331e13d3dda840222c75a3
Pieter Timmerman <p.h.timmerman@st.hanze.nl>
2020-11-04 11:47:43 +0100

    Tabs plus menu

--- GUI.py
+++ GUI.py
@@ -6,10 +6,10 @@ from tkinter import Tk
 #functions
 class Root(Tk):
     def __init__(self):
-    #window
+        #window
         super(Root,self).__init__()
-        self = tk.Tk()
-        self.geometry("700x500")
+        self.title("Rolluik Legend")
+        self.minsize(700,500)
 
         #menus
         menubar = tk.Menu(self)
@@ -26,12 +26,23 @@ class Root(Tk):
         self.tab3 = ttk.Frame(tabControl)
         tabControl.add(self.tab3, text="Instellingen")
         tabControl.pack(expand=1,fill="both")
-         #start GUI
-        
 
+        self.addingWidgets()
+        #self.addingTabs()
+    
     def addingWidgets(self):
-        labelFrame = ttk.LabelFrame(self.tab1, text="first")
-        labelFrame.grid(column = 0, row = 0, padx = 8, pady = 10)
+        labelFrame1 = ttk.LabelFrame(self.tab1, text="first")
+        labelFrame1.grid(column = 0, row = 0, padx = 8, pady = 10)
+        label = ttk.Label(labelFrame1,text= "Enter your Name:")
+        label.grid(column = 0, row = 0, sticky='W')
+
+    def addingTabs(self):
+        
+        tabControl1 = ttk.Notebook(self.tab1)
+        self.tab4 = ttk.Frame(tabControl1)
+        tabControl1.add(self.tab4, text="Scherm 1")
+        self.tab5 = ttk.Frame(tabControl1)
+        tabControl1.add(self.tab5, text="Scherm2")
 
 if __name__ == '__main__':
     root = Root()
