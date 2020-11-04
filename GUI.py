2b06ceb6acdf03e37712ff60c3610967f5425bcb
Pieter Timmerman <p.h.timmerman@st.hanze.nl>
2020-11-04 11:19:39 +0100

    Tabs plus menu

--- GUI.py
+++ GUI.py
@@ -1,18 +1,38 @@
+#imports
 import tkinter as tk
-def About():
-    print("This is a simple example of a menu")
-    
-top = tk.Tk()
-top.geometry("700x500")
-menu = tk.Menu(top)
-top.config(menu=menu)
-filemenu = tk.Menu(menu)
-menu.add_cascade(label="Home", menu=filemenu)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=top.quit)
+from tkinter import ttk
+from tkinter import Tk
 
-helpmenu = tk.Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=About)
+#functions
+class Root(Tk):
+    def __init__(self):
+    #window
+        super(Root,self).__init__()
+        self = tk.Tk()
+        self.geometry("700x500")
 
-tk.mainloop()
+        #menus
+        menubar = tk.Menu(self)
+        menubar.add_command(label="Scherm 1")
+        menubar.add_command(label="Scherm 2")
+        self.config(menu=menubar)
+
+        #tabs
+        tabControl = ttk.Notebook(self)
+        self.tab1 = ttk.Frame(tabControl)
+        tabControl.add(self.tab1, text="Home")
+        self.tab2 = ttk.Frame(tabControl)
+        tabControl.add(self.tab2, text="Grafieken")
+        self.tab3 = ttk.Frame(tabControl)
+        tabControl.add(self.tab3, text="Instellingen")
+        tabControl.pack(expand=1,fill="both")
+         #start GUI
+        
+
+    def addingWidgets(self):
+        labelFrame = ttk.LabelFrame(self.tab1, text="first")
+        labelFrame.grid(column = 0, row = 0, padx = 8, pady = 10)
+
+if __name__ == '__main__':
+    root = Root()
+    root.mainloop()
