0b19f0b8a50d89e704cbfce6f616a329f46d3c1b
Pieter Timmerman <p.h.timmerman@st.hanze.nl>
2020-11-04 11:59:52 +0100

    tabs correct ingesteld

--- GUI.py
+++ GUI.py
@@ -11,38 +11,29 @@ class Root(Tk):
         self.title("Rolluik Legend")
         self.minsize(700,500)
 
-        #menus
-        menubar = tk.Menu(self)
-        menubar.add_command(label="Scherm 1")
-        menubar.add_command(label="Scherm 2")
-        self.config(menu=menubar)
-
         #tabs
         tabControl = ttk.Notebook(self)
         self.tab1 = ttk.Frame(tabControl)
-        tabControl.add(self.tab1, text="Home")
+        tabControl.add(self.tab1, text="Scherm 1")
         self.tab2 = ttk.Frame(tabControl)
-        tabControl.add(self.tab2, text="Grafieken")
-        self.tab3 = ttk.Frame(tabControl)
-        tabControl.add(self.tab3, text="Instellingen")
+        tabControl.add(self.tab2, text="Scherm 2")
         tabControl.pack(expand=1,fill="both")
 
-        self.addingWidgets()
-        #self.addingTabs()
+        self.addingSubTabs(self.tab1)
+        self.addingSubTabs(self.tab2)
     
-    def addingWidgets(self):
-        labelFrame1 = ttk.LabelFrame(self.tab1, text="first")
-        labelFrame1.grid(column = 0, row = 0, padx = 8, pady = 10)
-        label = ttk.Label(labelFrame1,text= "Enter your Name:")
-        label.grid(column = 0, row = 0, sticky='W')
-
-    def addingTabs(self):
-        
-        tabControl1 = ttk.Notebook(self.tab1)
+    def addingSubTabs(self, tab):
+        labelFrame1 = ttk.LabelFrame(tab, text="first")
+        labelFrame1.grid(column = 0, row = 0, padx = 0, pady = 0)
+        tabControl1 = ttk.Notebook(tab)
+        self.tab3 = ttk.Frame(tabControl1)
+        tabControl1.add(self.tab3, text="Home")
         self.tab4 = ttk.Frame(tabControl1)
-        tabControl1.add(self.tab4, text="Scherm 1")
+        tabControl1.add(self.tab4, text="Grafieken")
         self.tab5 = ttk.Frame(tabControl1)
-        tabControl1.add(self.tab5, text="Scherm2")
+        tabControl1.add(self.tab5, text="Instellingen")
+        tabControl1.grid(column = 0, row = 0)
+        
 
 if __name__ == '__main__':
     root = Root()
